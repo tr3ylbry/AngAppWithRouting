7ebe687b1f70ee5a812e6884be36d3cbf4d27ef7
Trey Bryant <tbryant@aurasolutionsllc.com>
2021-01-25 10:07:07 -0700

    navbar implemented in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngWithRoutingAssn';
+  title = 'Angular App With Routing Assignment';
 }
